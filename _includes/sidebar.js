8ad621a2979a55706dbc2b481223078e8bb6f9a3
Don McCurdy <don.r.mccurdy@gmail.com>
2016-04-17 23:09:44 -0400

    Manually obfuscate email addresses

--- _includes/sidebar.js
+++ _includes/sidebar.js
@@ -1,18 +1,27 @@
 (function(document) {
-	var toggle = document.querySelector('.sidebar-toggle');
-	var sidebar = document.querySelector('#sidebar');
-	var checkbox = document.querySelector('#sidebar-checkbox');
+  var toggle = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('#sidebar');
+  var checkbox = document.querySelector('#sidebar-checkbox');
 
-	function toggleSidebar(e) {
-		var target = e.target;
+  function toggleSidebar(e) {
+    var target = e.target;
 
-		if(!checkbox.checked ||
-			sidebar.contains(target) ||
-			(target === checkbox || target === toggle)) return;
+    if(!checkbox.checked ||
+      sidebar.contains(target) ||
+      (target === checkbox || target === toggle)) return;
 
-		checkbox.checked = false;
-	}
+    checkbox.checked = false;
+  }
 
-	document.addEventListener('click', toggleSidebar, false);
-	document.addEventListener('touchstart', toggleSidebar, false);
+  document.addEventListener('click', toggleSidebar, false);
+  document.addEventListener('touchstart', toggleSidebar, false);
+
+  // De-obfuscate email addresses.
+  var obfAnchors = document.querySelectorAll('[data-obf]');
+  obfAnchors = [].slice.call(obfAnchors);
+  for (var i = 0, s = ''; i < obfAnchors.length; i++) {
+    s = obfAnchors[i].getAttribute('data-obf').split('').reverse().join('');
+    obfAnchors[i].textContent = s;
+    if (obfAnchors[i].href) obfAnchors[i].href = 'mailto:' + s;
+  }
 })(document);
